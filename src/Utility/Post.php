7833179a5d1afb15c9c47d1663ff5d3a0f2a504e
Marisia <marisia@webomg.com>
2014-09-22 22:11:40 -0400

    replaced guzzle for native curl

--- src/Utility/Post.php
+++ src/Utility/Post.php
@@ -15,10 +15,19 @@ class Post
 	}
 	
 	public function send()
-	{
-		$response = \GuzzleHttp\post($this->url, ['body' => $this->data]);
-		$code = $response->getStatusCode();
-		if($code == 200)
+	{				
+		    $ch = curl_init($this->url);
+		    curl_setopt($ch, CURLOPT_HEADER, 0);
+		    curl_setopt($ch, CURLOPT_NOBODY, 0);
+			curl_setopt($ch, CURLOPT_POST, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $this->data);
+		    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT,60);
+			curl_setopt($ch, CURLOPT_TIMEOUT, 60); 
+			curl_setopt($ch, CURLOPT_MAXCONNECTS, true);	
+			$sResp = curl_exec($ch);
+	
+		if($sResp['http_code'] == 200)
 			return true;
 		return false;
 	}
